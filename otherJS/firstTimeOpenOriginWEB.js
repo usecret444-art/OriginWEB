0424d3bf0119ddeff909564d760a6b0c750d6172
Ben Doverin <usecret444@gmail.com>
2026-04-09 19:36:14 +0700

    Update firstTimeOpenOriginWEB.js

--- otherJS/firstTimeOpenOriginWEB.js
+++ otherJS/firstTimeOpenOriginWEB.js
@@ -1,6 +1,6 @@
 addNotification(
     "/OriginWEB/originData/iconPacks/origin_icon/system_settings.png",
     "OriginWEB",
-    `Welcome to OriginWEB V${vst} for the first time`,
+    `Welcome to DoverinOS V${vst}, This Version is Currently on Developer Beta so if you have any Bugs Report it @BenDoverin2 via Telegram!`,
     "app_settings"
 );
